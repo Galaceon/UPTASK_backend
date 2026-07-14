f01cf0b8a3d17c3044de6f59f7ee32caaf63051a
Galaceon <antoniogarcialtr@gmail.com>
2026-07-15 01:01:59 +0200

    creating endpoint to delete project

--- src/controllers/ProjectController.ts
+++ src/controllers/ProjectController.ts
@@ -61,4 +61,22 @@ export class ProjectController {
             console.log(error)
         }
     }
+
+    static deleteProject = async (req: Request, res: Response) => {
+        const {id} = req.params
+
+        try {
+            const project = await Project.findById(id, req.body)
+
+            if(!project) {
+                const error = new Error('Proyecto no encontrado')
+                return res.status(404).json({error: error.message})
+            }
+            
+            await project.deleteOne()
+            res.send('Proyecto eliminado correctamente')
+        } catch (error) {
+            console.log(error)
+        }
+    }
 }
