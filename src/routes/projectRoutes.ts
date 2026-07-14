ebc0cda5538ae4b32794e159f5b715f9b0b4180a
Galaceon <antoniogarcialtr@gmail.com>
2026-07-15 00:34:33 +0200

    creating endpoint to update a project

--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -21,6 +21,19 @@ router.get('/', ProjectController.getAllProjects)
 router.get('/:id', 
     param('id').isMongoId().withMessage('ID no válido'),
     handleInputErrors,
-    ProjectController.getProjectById)
+    ProjectController.getProjectById
+)
+
+router.put('/:id', 
+    param('id').isMongoId().withMessage('ID no válido'),
+    body('projectName')
+        .notEmpty().withMessage('El nombre del proyecto es obligatorio'),
+    body('clientName')
+        .notEmpty().withMessage('El nombre del cliente es obligatorio'),
+    body('description')
+        .notEmpty().withMessage('La descripción del proyecto es obligatoria'),
+    handleInputErrors,
+    ProjectController.updateProject
+)
 
 export default router
