027e2208e517b5826f8b97deedc124d4d8cfbffe
Galaceon <antoniogarcialtr@gmail.com>
2026-07-15 23:20:22 +0200

    relating models, tasks, and projects

--- src/models/Project.ts
+++ src/models/Project.ts
@@ -1,6 +1,6 @@
 // Define el esquema de Mongoose y el tipo TypeScript para la colección de proyectos en la base de datos.
 
-import mongoose, {Schema, Document, PopulatedDoc} from "mongoose";
+import mongoose, {Schema, Document, PopulatedDoc, Types} from "mongoose";
 import { ITask } from "./Task";
 
 //typescript
@@ -8,7 +8,7 @@ export interface IProject extends Document {
     projectName: string
     clientName: string
     description: string
-    tasks: PopulatedDoc<ITask>
+    tasks: PopulatedDoc<ITask & Document>[]
 }
 
 //mongoose
@@ -27,8 +27,14 @@ const ProjectSchema : Schema = new Schema({
         type: String,
         required: true,
         trim: true,
-    }
-})
+    },
+    tasks: [
+        {
+            type: Types.ObjectId,
+            ref: 'Taks'
+        }
+    ]
+}, {timestamps: true})
 
 const Project = mongoose.model<IProject>('Project', ProjectSchema)
 export default Project
